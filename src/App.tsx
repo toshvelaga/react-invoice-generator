5415c4a31d0bdbd843ee088009a36145604de19f
Tosh Velaga <toshvelaga@gmail.com>
2021-09-09 22:58:19 -0500

    edit files

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import InvoicePage from './components/InvoicePage'
+import './scss/_app.css'
+import './scss/_color.css'
+import './scss/_icons.css'
+import './scss/_layout.css'
+import './scss/_spacing.css'
+import './scss/_text.css'
+import './scss/_typography.css'
 
 function App() {
   return (
-    <div className="app">
-      <h1 className="center fs-30">React Invoice Generator</h1>
+    <div className='app'>
+      <h1 className='center fs-30'>React Invoice Generator</h1>
       <InvoicePage />
     </div>
   )
